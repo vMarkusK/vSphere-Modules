76c28a4c794a1496a9e9481dd39aad5ec2f5720a
mycloudrevolution <mycloudrevolution@users.noreply.github.com>
2017-01-18 10:41:33 +0100

    Added Summary / error handling

--- Get-NewAndRemovedVMs.psm1
+++ Get-NewAndRemovedVMs.psm1
@@ -8,12 +8,13 @@ function Get-NewAndRemovedVMs {
     ===========================================================================
     Changelog:  
     2016.12 ver 1.0 Base Release 
+	2017.01 ver 1.1 Added Summary
     ===========================================================================
     External Code Sources:  
     https://github.com/alanrenouf/vCheck-vSphere
     ===========================================================================
     Tested Against Environment:
-    vSphere Version: 5.5 U2
+    vSphere Version: 5.5 U2, 6.5
     PowerCLI Version: PowerCLI 6.3 R1, PowerCLI 6.5 R1
     PowerShell Version: 4.0, 5.0
     OS Version: Windows 8.1, Server 2012 R2
@@ -33,6 +34,9 @@ function Get-NewAndRemovedVMs {
     .PARAMETER ClusterName
     Name or Wildcard of your vSphere Cluster Name(s) to report.
 
+	.PARAMETER Summary
+    Displays only a summary instead of teh whole report.
+
     .PARAMETER Day
     Range in Days to report.
 
@@ -45,11 +49,13 @@ param(
     [Parameter(Mandatory=$True, ValueFromPipeline=$False, Position=0, HelpMessage = "Name or Wildcard of your vSphere Cluster Name to report")]
     [ValidateNotNullorEmpty()]
         [String]$ClusterName,
-    [Parameter(Mandatory=$False, ValueFromPipeline=$False, Position=1, HelpMessage = "Range in Days to report")]
-    [ValidateNotNullorEmpty()]
-        [String]$Days = "30"
+	[Parameter(Mandatory=$False, ValueFromPipeline=$False, Position=1, HelpMessage = "Displays only a summary")]
+		[Switch]$Summary,
+    [Parameter(Mandatory=$False, ValueFromPipeline=$False, Position=2, HelpMessage = "Range in Days to report")]
+		[String]$Days = "30"
 )
 Begin {
+	If (!(Get-Cluster -Name $ClusterName -ErrorAction SilentlyContinue)) {throw "No vSphere Cluster matching the Name '$ClusterName' found."}
     function Get-VIEventPlus {
 	 
 	param(
@@ -124,8 +130,22 @@ Begin {
 }
 
 process {
-    $result = Get-VIEventPlus -Start ((get-date).adddays(-$Days)) -EventType @("VmCreatedEvent", "VmBeingClonedEvent", "VmBeingDeployedEvent","VmRemovedEvent")
-    $sortedResult = $result | Select CreatedTime, @{N='Cluster';E={$_.ComputeResource.Name}}, @{Name="VMName";Expression={$_.vm.name}}, UserName, @{N='Type';E={$_.GetType().Name}}, FullFormattedMessage | Sort CreatedTime
-    $sortedResult | where {$_.Cluster -like $ClusterName}
+    If ($Summary) {
+		$output = $sortedResult | where {$_.Cluster -like $ClusterName}
+		$Report = [PSCustomObject] @{
+							Created 	= (Get-VIEventPlus -Start ((get-date).adddays(-$Days)) -EventType @("VmCreatedEvent") | where {$_.ComputeResource.Name -like $ClusterName}).count
+							Cloned 		= (Get-VIEventPlus -Start ((get-date).adddays(-$Days)) -EventType @("VmBeingClonedEvent") | where {$_.ComputeResource.Name -like $ClusterName}).count
+							Deployed 	= (Get-VIEventPlus -Start ((get-date).adddays(-$Days)) -EventType @("VmBeingDeployedEvent") | where {$_.ComputeResource.Name -like $ClusterName}).count
+							Removed 	= (Get-VIEventPlus -Start ((get-date).adddays(-$Days)) -EventType @("VmRemovedEvent") | where {$_.ComputeResource.Name -like $ClusterName}).count
+
+						}
+		$Report
+	}
+	else {
+		$result = Get-VIEventPlus -Start ((get-date).adddays(-$Days)) -EventType @("VmCreatedEvent", "VmBeingClonedEvent", "VmBeingDeployedEvent","VmRemovedEvent")
+		$sortedResult = $result | Select CreatedTime, @{N='Cluster';E={$_.ComputeResource.Name}}, @{Name="VMName";Expression={$_.vm.name}}, UserName, @{N='Type';E={$_.GetType().Name}}, FullFormattedMessage | Sort CreatedTime
+		$sortedResult | where {$_.Cluster -like $ClusterName}
+	}
+	
 }
 }
